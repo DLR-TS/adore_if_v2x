b20f3979ec1dd0b8bf324eb9fc73b1e6999122f9
Daniel Hess <Daniel.Hess@DLR.de>
2022-07-06 22:52:50 +0200

    decoupled denm_to_checkpoint

--- adore_if_v2x/src/denm_to_checkpoint.cpp
+++ adore_if_v2x/src/denm_to_checkpoint.cpp
@@ -13,10 +13,11 @@
  *   Daniel Heß - convert output to checkpoint message
  ********************************************************************************/
 #include <ros/ros.h>
-#include <adore_if_ros/baseapp.h>
-#include <adore/env/afactory.h>
-#include <adore/mad/coordinateconversion.h>
+#include <coordinate_conversion/coordinate_conversion.h>
 #include <denm_v2_denm_pdu_descriptions/DENM.h>
+#include <adore_if_ros_msg/TCDConnectionStateTrace.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Point.h>
 
 
 /*
@@ -26,24 +27,24 @@ namespace adore
 {
 namespace if_ROS
 {
-class DENM2Checkpoint : public Baseapp
+class DENM2Checkpoint 
 {
   private: 
   ros::Subscriber DENMSubscriber_;
   ros::Subscriber odomSubscriber_;
-  adore::env::AFactory::TControlledConnectionWriter* checkpoint_writer_;
+  ros::Publisher publisher_;
   double time_;
   
   public:
   void init(int argc, char **argv, double rate, std::string nodename)
   {        
-    Baseapp::init(argc, argv, rate, nodename);
-    Baseapp::initSim();
-    std::function<void()> run_fcn = (std::bind(&DENM2Checkpoint::run_func, this)); 
-    Baseapp::addTimerCallback(run_fcn);
-    adore::if_ROS::ENV_Factory env_factory(getRosNodeHandle());
-    checkpoint_writer_ = env_factory.getCheckPointWriter();
+    ros::init(argc, argv, nodename);
+    ros::NodeHandle nh;
+    DENMSubscriber_= nh.subscribe("v2x/incoming/DENM",1,&DENM2Checkpoint::receive_denm,this);
+    odomSubscriber_= nh.subscribe("localization",1,&DENM2Checkpoint::receive_odom,this);
+    publisher_ = nh.advertise<adore_if_ros_msg::TCDConnectionStateTrace>("ENV/checkpoints",10);
     time_ = 0.0;
+    ros::spin();
   }
   void receive_denm(denm_v2_denm_pdu_descriptions::DENM msg)
   {
@@ -61,24 +62,29 @@ class DENM2Checkpoint : public Baseapp
             32, x, y
         );
         z = msg.denm.management.eventPosition.altitude.altitudeValue.value;
-        adore::env::ControlledConnection con(x,y,z,x,y,z);
+        adore_if_ros_msg::TCDConnectionStateTrace result;
+        result.connection.first.x = x;
+        result.connection.first.y = y;
+        result.connection.first.z = z;
+        result.connection.last = result.connection.first;
         double end_time = time_+2.0;
         double open_end = end_time + 10000.0;
-        adore::env::ConnectionStateEvent e0(adore::env::ConnectionState::STOP___AND___REMAIN,end_time,false,false,0.0,0.0);
-        adore::env::ConnectionStateEvent e1(adore::env::ConnectionState::PROTECTED___MOVEMENT___ALLOWED,open_end,false,false,0.0,0.0);
-        con.insertStateEvent(e0);
-        con.insertStateEvent(e1);
-        checkpoint_writer_->write(con);
+        adore_if_ros_msg::TCDConnectionState s0,s1;
+        s0.state = adore_if_ros_msg::TCDConnectionState::STOP___AND___REMAIN;
+        s0.minEndTime = end_time;
+        s0.maxEndTime_present = false;
+        s0.likelyTime_present = false;
+        s1 = s0;
+        s1.minEndTime = open_end;
+        s1.state = adore_if_ros_msg::TCDConnectionState::PROTECTED___MOVEMENT___ALLOWED;
+        result.data.push_back(s0);
+        result.data.push_back(s1);
+        publisher_.publish(result);
     }
   }
   void receive_odom(nav_msgs::OdometryConstPtr msg)
   {
       time_ = msg->header.stamp.toSec();
-  }
-  virtual void run_func()
-  {
-    DENMSubscriber_= getRosNodeHandle()->subscribe("v2x/incoming/DENM",1,&DENM2Checkpoint::receive_denm,this);
-    odomSubscriber_= getRosNodeHandle()->subscribe("localization",1,&DENM2Checkpoint::receive_odom,this);
   }
 };
 } // namespace if_ROS
@@ -92,7 +98,6 @@ int main(int argc,char **argv)
 
     adore::if_ROS::DENM2Checkpoint denm_checkpoint;
     denm_checkpoint.init(argc, argv, 1., "denm2checkpoint_node");
-    denm_checkpoint.run();
     return 0;
 
 }
