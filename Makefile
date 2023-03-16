70ee801bb74998c901bbff30f0d849eefe2a6841
Andrew Koerner <andrew.koerner@dlr.de>
2023-03-16 08:43:00 +0100

    WIP: configurable submodules

--- Makefile
+++ Makefile
@@ -8,35 +8,26 @@ MAKEFILE_PATH:=$(shell dirname "$(abspath "$(lastword $(MAKEFILE_LIST)"))")
 MAKEFLAGS += --no-print-directory
 
 include adore_if_v2x.mk
-include ${ADORE_IF_V2X_SUBMODULES_PATH}/adore_if_ros_msg/make_gadgets/make_gadgets.mk
-include ${ADORE_IF_V2X_SUBMODULES_PATH}/adore_if_ros_msg/make_gadgets/docker/docker-tools.mk
-include ${ADORE_IF_V2X_SUBMODULES_PATH}/apt_cacher_ng_docker/apt_cacher_ng_docker.mk
 
 .EXPORT_ALL_VARIABLES:
 DOCKER_BUILDKIT?=1
 DOCKER_CONFIG?=
 
-include ${ADORE_IF_V2X_SUBMODULES_PATH}/adore_if_ros_msg/adore_if_ros_msg.mk
-include ${ADORE_IF_V2X_SUBMODULES_PATH}/coordinate_conversion/coordinate_conversion.mk
-include ${ADORE_IF_V2X_SUBMODULES_PATH}/v2x_if_ros_msg/v2x_if_ros_msg.mk
-
 .PHONY: all
-all: build
+all: help
 
 .PHONY: set_env 
 set_env: 
 	$(eval PROJECT := ${ADORE_IF_V2X_PROJECT}) 
 	$(eval TAG := ${ADORE_IF_V2X_TAG})
 
-
 .PHONY: build
 build: set_env start_apt_cacher_ng
-	$(eval MAKE_GADGETS_MAKEFILE_PATH := $(shell unset MAKE_GADGETS_MAKEFILE_PATH))
 	rm -rf ${ROOT_DIR}/${PROJECT}/build
-	cd ${ADORE_IF_V2X_SUBMODULES_PATH}/adore_if_ros_msg && make 
-	cd ${ADORE_IF_V2X_SUBMODULES_PATH}/v2x_if_ros_msg && make 
-	cd ${ADORE_IF_V2X_SUBMODULES_PATH}/plotlablib && make 
-	cd ${ADORE_IF_V2X_SUBMODULES_PATH}/coordinate_conversion && make 
+	cd ${ADORE_IF_V2X_SUBMODULES_PATH}/adore_if_ros_msg && make build 
+	cd ${ADORE_IF_V2X_SUBMODULES_PATH}/v2x_if_ros_msg && make build 
+	cd ${ADORE_IF_V2X_SUBMODULES_PATH}/plotlablib && make build 
+	cd ${ADORE_IF_V2X_SUBMODULES_PATH}/coordinate_conversion && make build 
 	cd "${ROOT_DIR}" && \
     touch CATKIN_IGNORE
 	docker build --network host \
@@ -49,8 +40,15 @@ build: set_env start_apt_cacher_ng
 	cd "${ROOT_DIR}" && \
 	docker cp $$(docker create --rm ${PROJECT}:${TAG}):/tmp/${PROJECT}/${PROJECT}/build "${ROOT_DIR}/${PROJECT}"
 
+.PHONY: clean_submodules
+clean_submodules:
+	cd "${ADORE_IF_V2X_SUBMODULES_PATH}/adore_if_ros_msg" && make clean 
+	cd "${ADORE_IF_V2X_SUBMODULES_PATH}/v2x_if_ros_msg" && make clean 
+	cd "${ADORE_IF_V2X_SUBMODULES_PATH}/plotlablib" && make clean 
+	cd "${ADORE_IF_V2X_SUBMODULES_PATH}/coordinate_conversion" && make clean 
+
 .PHONY: clean 
-clean: set_env 
+clean: set_env clean_submodules 
 	rm -rf ${ROOT_DIR}/${PROJECT}/build
 	docker rm $$(docker ps -a -q --filter "ancestor=${PROJECT}:${TAG}") --force 2> /dev/null || true
 	docker rmi $$(docker images -q ${PROJECT}:${TAG}) --force 2> /dev/null || true
--- adore_if_v2x.mk
+++ adore_if_v2x.mk
@@ -1,6 +1,6 @@
 # This Makefile contains useful targets that can be included in downstream projects.
 
-ifndef adore_if_v2x_MAKEFILE_PATH
+ifeq ($(filter adore_if_v2x.mk, $(notdir $(MAKEFILE_LIST))), adore_if_v2x.mk)
 
 MAKEFLAGS += --no-print-directory
 
@@ -11,30 +11,28 @@ ADORE_IF_V2X_MAKEFILE_PATH:=$(strip $(shell realpath "$(shell dirname "$(lastwor
 ifeq ($(SUBMODULES_PATH),)
 ADORE_IF_V2X_SUBMODULES_PATH:=${ADORE_IF_V2X_MAKEFILE_PATH}
 else
-ADORE_IF_V2X_SUBMODULES_PATH:=$(SUBMODULES_PATH)
+ADORE_IF_V2X_SUBMODULES_PATH:=$(shell realpath ${SUBMODULES_PATH})
 endif
 
-MAKE_GADGETS_PATH:=${ADORE_IF_V2X_SUBMODULES_PATH}/adore_if_ros_msg/make_gadgets
+MAKE_GADGETS_PATH:=${ADORE_IF_V2X_SUBMODULES_PATH}/make_gadgets
 ifeq ($(wildcard $(MAKE_GADGETS_PATH)),)
-$(info INFO: To clone submodules use: 'git submodules update --init --recursive')
-$(info INFO: To specify alternative path for submodules use: SUBMODULES_PATH="<path to submodules>" make build')
-$(info INFO: Default submodule path is: ${ADORE_IF_V2X_MAKEFILE_PATH}')
-$(error "ERROR: ${MAKE_GADGETS_PATH} does not exist. Did you clone the submodules?")
+    $(info INFO: To clone submodules use: 'git submodules update --init --recursive')
+    $(info INFO: To specify alternative path for submodules use: SUBMODULES_PATH="<path to submodules>" make build')
+    $(info INFO: Default submodule path is: ${ADORE_IF_V2X_MAKEFILE_PATH}')
+    $(error "ERROR: ${MAKE_GADGETS_PATH} does not exist. Did you clone the submodules?")
 endif
+APT_CACHER_NG_DOCKER_PATH:=${ADORE_IF_V2X_SUBMODULES_PATH}/apt_cacher_ng_docker
 REPO_DIRECTORY:=${ADORE_IF_V2X_MAKEFILE_PATH}
 
-
 ADORE_IF_V2X_TAG:=$(shell cd ${MAKE_GADGETS_PATH} && make get_sanitized_branch_name REPO_DIRECTORY=${REPO_DIRECTORY})
-
 ADORE_IF_V2X_IMAGE:=${ADORE_IF_V2X_PROJECT}:${ADORE_IF_V2X_TAG}
-ADORE_IF_V2X_IMAGE:=${ADORE_IF_V2X_IMAGE}
 
 ADORE_IF_V2X_CMAKE_BUILD_PATH="${ADORE_IF_V2X_PROJECT}/build"
-
 ADORE_IF_V2X_CMAKE_INSTALL_PATH="${ADORE_IF_V2X_CMAKE_BUILD_PATH}/install"
 
 $(info ADORE_IF_V2X_SUBMODULES_PATH: ${ADORE_IF_V2X_SUBMODULES_PATH})
 include ${MAKE_GADGETS_PATH}/make_gadgets.mk
+include ${APT_CACHER_NG_DOCKER_PATH}/apt_cacher_ng_docker.mk
 include ${ADORE_IF_V2X_SUBMODULES_PATH}/plotlablib/plotlablib.mk
 include ${ADORE_IF_V2X_SUBMODULES_PATH}/coordinate_conversion/coordinate_conversion.mk
 include ${ADORE_IF_V2X_SUBMODULES_PATH}/v2x_if_ros_msg/v2x_if_ros_msg.mk
@@ -43,7 +41,7 @@ include ${ADORE_IF_V2X_SUBMODULES_PATH}/v2x_if_ros_msg/v2x_if_ros_msg.mk
 
 .PHONY: build_adore_if_v2x 
 build_adore_if_v2x: ## Build adore_if_v2x
-	cd "${ADORE_IF_V2X_MAKEFILE_PATH}" && make
+	cd "${ADORE_IF_V2X_MAKEFILE_PATH}" && make build
 
 .PHONY: clean_adore_if_v2x
 clean_adore_if_v2x: ## Clean adore_if_v2x build artifacts
@@ -57,7 +55,4 @@ branch_adore_if_v2x: ## Returns the current docker safe/sanitized branch for ado
 image_adore_if_v2x: ## Returns the current docker image name for adore_if_v2x
 	@printf "%s\n" ${ADORE_IF_V2X_IMAGE}
 
-.PHONY: update_adore_if_v2x
-update_adore_if_v2x:
-	cd "${ADORE_IF_V2X_MAKEFILE_PATH}" && git pull
 endif
